a16a460cc5ccf53b08c595c406e0cf922bab2425
rikuyaMaster <108648258+rikuyaMaster@users.noreply.github.com>
2022-07-04 16:03:51 +0900

    スプレッド構文追加

--- src/index.js
+++ src/index.js
@@ -89,5 +89,35 @@
  *
  */
 // name =の後がデフォルト値
-const sayHello = (name = "guest") => console.log(`こんにちは${name}さん！`);
-sayHello("安井");
+// const sayHello = (name = "guest") => console.log(`こんにちは${name}さん！`);
+// sayHello("安井");
+
+/**
+ *スプレッド構文 ...を記述するもの
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // // 配列の中身を展開
+// // console.log(...arr1);
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// // 分割代入
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+// コピー
+const arr6 = [...arr4];
+console.log(arr6);
+// 結合（+でできた配列とはまた別）
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
